378577b7a3fbe55acecf28c8c57373527838a718
ktekchan <ktekchan@buffalo.edu>
2015-05-23 13:29:46 -0400

    Added src files

---
+++ Main.java
@@ -0,0 +1,72 @@
+
+import java.util.Date;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+//import Stock_Map1.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {		
+		long start = new Date().getTime();		
+		Configuration conf = new Configuration();
+		
+	     Job job = Job.getInstance();
+	     job.setJarByClass(Stock_Map1.class);
+	     Job job2 = Job.getInstance();
+	     job2.setJarByClass(Stock_Map2.class);
+	     Job job3 = Job.getInstance();
+	     job3.setJarByClass(Stock_Map3.class);
+		 
+
+		System.out.println("\n---------------------Stock Volatility Begins---------------------\n");
+
+		job.setJarByClass(Stock_Map1.class);
+		job.setMapperClass(Stock_Map1.Mapper1.class);
+		job.setReducerClass(Stock_Map1.Reducer1.class);
+		
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+	
+		job2.setJarByClass(Stock_Map2.class);
+		job2.setMapperClass(Stock_Map2.Mapper2.class);
+		job2.setReducerClass(Stock_Map2.Reducer2.class);
+
+		job2.setMapOutputKeyClass(Text.class);
+		job2.setMapOutputValueClass(Text.class);
+
+		job3.setJarByClass(Stock_Map3.class);
+		job3.setMapperClass(Stock_Map3.Mapper3.class);
+		job3.setReducerClass(Stock_Map3.Reducer3.class);
+		
+		job3.setMapOutputKeyClass(Text.class);
+		job3.setMapOutputValueClass(Text.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]+"Inter_1"));
+		FileInputFormat.addInputPath(job2, new Path(args[1]+"Inter_1"));
+		
+		FileOutputFormat.setOutputPath(job2, new Path(args[1]+"Inter_2"));
+		FileInputFormat.addInputPath(job3, new Path(args[1]+"Inter_2"));
+		FileOutputFormat.setOutputPath(job3, new Path(args[1]+"output"));
+	
+		
+		job.waitForCompletion(true);
+		job2.waitForCompletion(true);
+		boolean status1 = job3.waitForCompletion(true);
+		if (status1 == true) {
+			long end = new Date().getTime();
+			System.out.println("\nJob took " + (end - start) + "milliseconds\n");
+			System.out.println("\nJob took " + (end-start)/1000 + "seconds\n");
+		}	
+		System.out.println("Status: "+status1);
+		System.out.println("\n---------------------Stock Volatility Map-Reduce Ends---------------------\n");
+	}
+}
+
---
+++ Stock_Map1.java
@@ -0,0 +1,133 @@
+import java.io.*;
+import java.net.URI;
+import java.util.*;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+public class Stock_Map1{
+
+	public static class Mapper1 extends Mapper<Object, Text, Text, Text>{
+		private Text key1 = new Text();
+		private Text value1 = new Text();
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+
+			int ignore_first = 0;
+			long length;
+				
+			//to make a note of the filename
+			FileSplit fileSplit = (FileSplit)context.getInputSplit();
+			String filename = (fileSplit.getPath().getName());
+				
+			length = fileSplit.getLength();
+			if (length == 0){}
+			else{
+				//Read a line
+				String line = value.toString();				
+			
+				//Parse the csv file one line at a time
+				String columns[] = null;
+				columns = line.split(",");				
+				if (columns[0].equals("Date")){
+
+				}
+
+				else{
+				
+					//splits the date by '-'
+					String date[] = null;
+					date = columns[0].split("-");				
+				
+					//stores filename appended with month and year
+					key1.set(filename+"/"+date[1]+date[0]);
+				
+					//stores adjusted closed price appended with the date
+					value1.set(columns[6]+"/"+date[2]);
+				
+					context.write(key1, value1);
+				}
+
+			}
+
+		}	
+}
+
+
+	public static class Reducer1 extends Reducer<Text, Text, Text, Text>{
+		
+		private Text valToWrite = null;
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+		
+			ArrayList<ArrayList<String>> valAndDate = new ArrayList<ArrayList<String>>();
+			String valComponents[] = null;
+			int indexCount = 0;
+			
+			for(Text value: values){
+				
+				//read the value and remove date from it
+				String valClose = new String();
+				valClose = value.toString();
+				
+				valComponents = valClose.split("/");
+								
+				//2d Arraylist with 1st column having the date(day) and 2nd column having the closing value
+	
+				try{
+					ArrayList<String> temp = new ArrayList<String>();
+					temp.add(0, valComponents[1]);
+					temp.add(1, valComponents[0]);
+					valAndDate.add(temp);
+					indexCount++;
+				}
+				catch(ArrayIndexOutOfBoundsException e){
+					System.out.println("Print Stack trace!!");
+					e.printStackTrace();
+				}
+				catch(IndexOutOfBoundsException e){
+					System.out.println("Print Index Stack trace!!");
+					e.printStackTrace();
+				}
+				
+			}
+
+			//Sort the arraylist according to the date
+			Collections.sort(valAndDate, new Comparator<ArrayList<String>>() {    
+        		@Override
+		        public int compare(ArrayList<String> o1, ArrayList<String> o2) {
+		            		return o1.get(0).compareTo(o2.get(0));
+		        	}               
+			});
+
+			
+			double val1;
+			double val2;
+			double Xi;
+			
+			//Calculate xi
+			val1 = Double.parseDouble(valAndDate.get(0).get(1)); 
+			val2 = Double.parseDouble(valAndDate.get(indexCount-1).get(1));
+			Xi = (val2-val1)/val1;
+			valToWrite = new Text(String.valueOf(Xi));
+
+			//Clear Arraylist
+			valAndDate.clear();
+		
+			//write key and xi
+			context.write(key, valToWrite); 
+			
+		}
+	}
+}
+
+
